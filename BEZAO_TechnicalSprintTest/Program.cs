74af048f44cece92e87a26a87d192c938f3a10d2
Ebube4im Anya <ebube4im@gmail.com>
2022-10-03 11:42:18 +0100

    Added Secret Answer Guessing Class

--- BEZAO_TechnicalSprintTest/Program.cs
+++ BEZAO_TechnicalSprintTest/Program.cs
@@ -4,5 +4,18 @@ using BEZAO_TechnicalSprintTest;
 Console.WriteLine("BEZAO TECHNICAL TEST");
 
 
-LeapYearPrinter leapYear = new LeapYearPrinter();
-leapYear.Print();
+//LeapYearPrinter leapYear = new LeapYearPrinter();
+//leapYear.Print();
+
+SecretAnswerGuess secretAnswer = new SecretAnswerGuess();
+int guessCount = 0;
+while (guessCount != 20)
+{
+    guessCount++;
+    Console.WriteLine("Can you guess our secret number? Enter any number and we will tell you how close you are.");
+    secretAnswer.checkNumber(int.Parse(Console.ReadLine()));
+    Console.WriteLine(guessCount.ToString());
+
+}
+ 
+
---
+++ BEZAO_TechnicalSprintTest/SecretAnswerGuess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEZAO_TechnicalSprintTest
+{
+    internal class SecretAnswerGuess
+    {
+        int SecretNumber = 25;
+
+        public void checkNumber(int number)
+        {
+
+            if (number > SecretNumber)
+            {
+                Console.WriteLine("Your guess " + number + " is too large");
+            }
+            else if (number < SecretNumber)
+                Console.WriteLine("Your guess " + number + " is too small");
+            else if (number == SecretNumber){
+                Console.WriteLine("Your guess " + number + " is Correct");
+            }
+            
+        }
+    }
+}
