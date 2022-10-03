737709026a5526cc2df4e07823530c42875d8315
Ebube4im Anya <ebube4im@gmail.com>
2022-10-03 13:10:20 +0100

    Updated Program.cs

---
+++ BEZAO_TechnicalSprintTest/PigLatin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEZAO_TechnicalSprintTest
+{
+    internal class PigLatin
+    {
+       public string wordToTranslate = "";
+        public string translatedWord = "";
+
+        public void Translate()
+        {
+          string[] newStringArray =  wordToTranslate.Split(' ');
+
+            List<string> newStringArray2 = new List<string>();
+
+            foreach (var item in newStringArray)
+            {
+                char firstLetter;
+                char lastLetter;
+                string newWord;
+
+                firstLetter = item.First();
+                lastLetter = item.Last();
+
+               newWord = item.Substring(1,item.Length - 1) + firstLetter + "ay";
+            
+                newStringArray2.Add(newWord);
+
+                
+            }
+
+            foreach (var item in newStringArray2)
+            {
+                translatedWord = translatedWord + item + " ";
+
+            }
+            Console.WriteLine(translatedWord);
+
+
+        }
+
+        public void TranslateBack()
+        {
+            wordToTranslate = "";
+            string[] newStringArray = translatedWord.Split(' ');
+
+            List<string> newStringArray2 = new List<string>();
+
+            foreach (var item in newStringArray)
+            {
+                
+                char lastLetter;
+                string newWord;
+
+                string replacedWord = item.Remove(item.Length - 2);
+
+                  
+                newWord = replacedWord.Last() + replacedWord.Remove(replacedWord.Length -1);
+
+                newStringArray2.Add(newWord);
+
+
+            }
+
+            foreach (var item in newStringArray2)
+            {
+                wordToTranslate = wordToTranslate + item + " "; 
+
+            }
+            Console.WriteLine(wordToTranslate);
+
+
+        }
+    }
+}
--- BEZAO_TechnicalSprintTest/Program.cs
+++ BEZAO_TechnicalSprintTest/Program.cs
@@ -3,19 +3,58 @@ using BEZAO_TechnicalSprintTest;
 
 Console.WriteLine("BEZAO TECHNICAL TEST");
 
+Console.WriteLine("This Application Constains three simple Programs. Choose which program to Run " +
+    "\n Enter 1 to Run Leap Year Printer \n Enter 2 to Run Secret Number Guesser \n Enter 3 to Run Pig Latin Translator");
 
-//LeapYearPrinter leapYear = new LeapYearPrinter();
-//leapYear.Print();
 
-SecretAnswerGuess secretAnswer = new SecretAnswerGuess();
-int guessCount = 0;
-while (guessCount != 20)
+string selectedresponse = "";
+
+selectedresponse = Console.ReadLine();
+
+if(selectedresponse != "" )
 {
-    guessCount++;
-    Console.WriteLine("Can you guess our secret number? Enter any number and we will tell you how close you are.");
-    secretAnswer.checkNumber(int.Parse(Console.ReadLine()));
-    Console.WriteLine(guessCount.ToString());
+    if (selectedresponse == "1")
+    {
+        Console.WriteLine("Welcome to Leap Year Printer Program");
+        LeapYearPrinter leapYear = new LeapYearPrinter();
+        leapYear.Print();
 
+    }
+    if (selectedresponse == "2")
+    {
+        Console.WriteLine("Welcome to Secret Answer Guesser Program");
+
+        SecretAnswerGuess secretAnswer = new SecretAnswerGuess();
+        int guessCount = 0;
+        while (guessCount != 20)
+        {
+            guessCount++;
+            Console.WriteLine("Can you guess our secret number? Enter any number and we will tell you how close you are.");
+            secretAnswer.checkNumber(int.Parse(Console.ReadLine()));
+            Console.WriteLine(guessCount.ToString());
+
+        }
+    }
+    if (selectedresponse == "3")
+    {
+        Console.WriteLine("Welcome to Pig Latin Translator Program");
+        PigLatin pigLatin = new PigLatin();
+
+        Console.WriteLine("Enter a sentence to translate");
+        pigLatin.wordToTranslate = Console.ReadLine();
+        pigLatin.Translate();
+
+        Console.WriteLine("To translate Pig Latin word back to English, Reply Yes");
+       
+        if (Console.ReadLine().ToUpper() == "YES")
+        {
+            pigLatin.TranslateBack();
+        }
+    
+
+    }
 }
- 
+
+
+
 
